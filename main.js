5f3566378af7eeefa23c47595b2437453317fb3b
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-09-29 12:39:01 -0300

    Projeto Task List v02 - Funcionalidade: adicionando, concluindo tarefas e clear all funcionando

--- main.js
+++ main.js
@@ -6,28 +6,53 @@ const list = document.getElementById('list');
 let id = 0;
 
 plusButton.addEventListener('click', () => {
-    console.log("Adicionar");
     createTask();
+    task.value='';
 ;})
 
 clearButton.addEventListener('click', () => {
     clearTask();
-    console.log("Apagar");
 ;})
+
+list.addEventListener('click', (evento) => {
+    const selected = evento.target;
+    console.log(evento);
+    console.log(evento.target);
+    console.log(evento.target.dataset);
+
+    selected.classList.toggle('marked');
+
+    // selected.style.cssText =
+    // 'text-decoration: line-through;'
+    
+;})
+
 
 theme.addEventListener('click', () => {
     theme.classList.toggle('dark-mode');
     document.body.classList.toggle('dark-mode');
 })
+
+
+
+
+
+
+
 function createTask(){
+    // Criado tag li que possui a classe task e o data-atributtes data-id
+
     const newTask = document.createElement("li");
     newTask.classList.add('task');
     newTask.setAttribute('data-id',id);
-    list.appendChild(newTask);
+    
     newTask.innerHTML = task.value;
+    
+    list.appendChild(newTask);
+
     id++;
-    task.value='';
 }
 function clearTask(){
-    list.removeChild(li);
+    const item = document.getElementsByTagName('li');
+    list.remove(item);
 }
